aa53ae8399a36f3106f30b5eb78b2d2597acac6e
Denis Wilton <35562940+DennisWilton@users.noreply.github.com>
2022-02-18 19:04:45 -0400

    Estrutura basica:

--- src/manager/MainManager.js
+++ src/manager/MainManager.js
@@ -1,7 +1,26 @@
-function createMain(){
-    const self = {};
+import { reactive } from 'vue';
 
-    return self;
+function createMain() {
+  const self = {};
+
+  self.players = reactive([
+    { id: 328173, name: 'Jogador padrão', skill: 1, distance: 1 },
+  ]);
+
+  self.addPlayer = (player) => {
+    self.players.push(player);
+  };
+
+  self.removePlayer = (player) => {
+    self.players.splice(
+      self.players.findIndex((p) => p.id === player.id),
+      1
+    );
+  };
+
+  window.main = self;
+
+  return self;
 }
 
 export default createMain();
